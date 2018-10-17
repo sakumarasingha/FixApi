b190da550feac763e4b10b4f1e17f7f429575b90
sakumarasingha <33798933+sakumarasingha@users.noreply.github.com>
2018-10-17 14:01:23 +0400

    Initial Commit

---
+++ JobApi/Models/Job.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Job
+{
+    public int Id { get; set; }
+    public string Code { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime PostDate { get; set; }
+    public DateTime ValidTill { get; set; }
+}
